305ed47b66bcedf87a503befc79a0b7e6bc593c1
Szymek <sprzepiora@hotmail.co.uk>
2020-05-13 14:13:02 +0200

    made Urls and updated views

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 from django.contrib.auth.models import User
+
+
+class PublishedManager(models.Manager):
+    def get_quert_set(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 
 class Post(models.Model):
@@ -15,11 +21,19 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
+    object = models.Manager()
+    published = PublishedManager()
+
     class Meta:
         ordering = ('-publish', )
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
+
+
 
 
 
